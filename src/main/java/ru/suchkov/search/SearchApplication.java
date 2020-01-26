d8e1aec33f93b828fc1eb6ed87c69d511c106af0
Денис Сучков <ny1828@yandex.ru>
2020-01-26 16:58:24 +0300

    fix search application

--- src/main/java/ru/suchkov/search/SearchApplication.java
+++ src/main/java/ru/suchkov/search/SearchApplication.java
@@ -2,7 +2,11 @@ package ru.suchkov.search;
 
 
 import ru.suchkov.search.index.Indexer;
+import ru.suchkov.search.search.Searcher;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 
 public class SearchApplication {
@@ -12,7 +16,7 @@ public class SearchApplication {
 			System.out.println("Нет ни одного аргумента");
 		} else {
 			String param0 = args[0].toLowerCase();
-			if ("--index".equals(param0) || "-i".equals(param0)) {
+			if ("--index".equals(param0) || "-index".equals(param0) || "-i".equals(param0)) {
 				if (args.length != 3) {
 					System.out.println("Необходимо ввести путь к индексируемому файлу " +
 							"и директорию для построения индекса");
@@ -20,7 +24,26 @@ public class SearchApplication {
 					new Indexer(Path.of(args[1]), Path.of(args[2])).build();
 				}
 			} else {
-				System.out.println("Неизвестная команда.");
+				if ("--search".equals(param0) || "-search".equals(param0) || "-s".equals(param0)) {
+					if (args.length != 3) {
+						System.out.println("Необходимо ввести путь к индексируемому файлу " +
+								"и директорию с построенныминдексом");
+					} else {
+						Searcher searcher = new Searcher(Path.of(args[1]), Path.of(args[2]));
+						try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+							System.out.print("Введите поисковый запрос $ ");
+							for (String line = reader.readLine();
+								 !line.toLowerCase().equals("exit()"); line = reader.readLine()) {
+								System.out.println(searcher.searchExpression(line));
+								System.out.print("$ ");
+							}
+						} catch (IOException e) {
+							System.out.println("Ошибка ввода");
+						}
+					}
+				} else {
+					System.out.println("Неизвестная команда.");
+				}
 			}
 		}
 	}
