abff825776b6c1bddae6f69376a4aecbfc09549b
Alex Kras <alexandrkrasovsky01@gmail.com>
2020-01-29 18:32:59 +0300

    begining of iverted index

--- src/main/java/app/index/Indexing.java
+++ src/main/java/app/index/Indexing.java
@@ -4,10 +4,25 @@ import app.structure.Document;
 import app.util.FileOperations;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Indexing {
     public static void runIndexing() {
         ArrayList<Document> docs = FileOperations.getDocsFromFile("./to_index.txt");
+
+        Map<String, Integer> termDocNumber = new HashMap<>();
+        Map<String, Integer> termPositionInDoc = new HashMap<>();
+
+        for (int i = 0; i < docs.size(); i++) {
+            Document doc = docs.get(i);
+            Map<String, Integer> termsAndPositions = doc.getTermsAndPositions();
+            for (String term : termsAndPositions.keySet()) {
+                termDocNumber.put(term, i);
+                // position
+
+            }
+        }
 
 
     }
