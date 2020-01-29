39ac1dec0ca560bab87b0a49c4be7fcbb99ba656
Alex Kras <alexandrkrasovsky01@gmail.com>
2020-01-29 12:19:42 +0300

    Main works, need to add search method

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,25 @@
 package app;
 
+import app.index.Indexing;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Write work mode: \"index\" or \"search\"\nAlso can use: \"i\" or \"s\"");
+            String mode = scanner.next();
+            if (mode.equals("index") || mode.equals("i")) {
+                Indexing.runIndexing();
+                break;
+            } else if (mode.equals("search") || mode.equals("s")) {
 
+                break;
+            } else {
+                System.out.println("Incorrect work mode");
+            }
+        }
     }
 }
