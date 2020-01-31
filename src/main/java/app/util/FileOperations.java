1a65295278117a1f11ee9ce46ff0da1cbaf33794
Alex Kras <alexandrkrasovsky01@gmail.com>
2020-01-31 16:01:18 +0300

    without add write, working now

--- src/main/java/app/util/FileOperations.java
+++ src/main/java/app/util/FileOperations.java
@@ -38,35 +38,7 @@ public class FileOperations {
     public static void invIndexToJson(InvertedIndex invIndex, String filename) {
         Gson gson = new Gson();
         File file = new File(filename);
-
-        InvertedIndex old = null;
-        FileReader fr;
-        BufferedReader bReader;
-
-        String jsonInvIndex = "";
-
-        if (file.canRead()) {
-            try {
-                fr = new FileReader(file);
-                bReader = new BufferedReader(fr);
-                old = gson.fromJson(bReader.readLine(), InvertedIndex.class);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            for (String term : invIndex.invIndex.keySet()) {
-                TermInv termInv = old.invIndex.getOrDefault(term, null);
-                if (termInv == null) {
-                    old.invIndex.put(term, invIndex.invIndex.get(term));
-                } else {
-                    old.invIndex.get(term).addAllDocs(invIndex.invIndex.get(term).getDocIds(), invIndex.invIndex.get(term).getPositionInDoc());
-                }
-            }
-            jsonInvIndex = gson.toJson(old);
-
-        } else {
-            jsonInvIndex = gson.toJson(invIndex);
-        }
-
+        String jsonInvIndex = gson.toJson(invIndex);
         try {
             FileWriter fw = new FileWriter(file);
             fw.write(jsonInvIndex);
