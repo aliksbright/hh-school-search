4db9a949bf337b46d0fbac485158e7aca8634978
Alex Kras <alexandrkrasovsky01@gmail.com>
2020-01-31 10:12:37 +0300

    add write into json

--- src/main/java/app/index/Indexing.java
+++ src/main/java/app/index/Indexing.java
@@ -4,7 +4,11 @@ import app.structure.Document;
 import app.structure.InvertedIndex;
 import app.structure.Term;
 import app.util.FileOperations;
+import com.google.gson.Gson;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,10 +19,22 @@ public class Indexing {
     public static void runIndexing() {
         ArrayList<Document> docs = FileOperations.getDocsFromFile("./to_index.txt");
 
+        HashMap<String, InvertedIndex> invIndex = getInvertedIndex(docs);
+
+        Gson gson = new Gson();
+        String jsonInvIndex = gson.toJson(invIndex);
+        try {
+            FileWriter fw = new FileWriter(new File("inv_index.json"));
+            fw.write(jsonInvIndex);
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public HashMap<String, InvertedIndex> getInvertedIndex(List<Document> docs) {
+    public static HashMap<String, InvertedIndex> getInvertedIndex(List<Document> docs) {
         HashMap<String, InvertedIndex> invIndex = new HashMap<>();
         for (int i = 0; i < docs.size(); i++) {
             Document doc = docs.get(i);
